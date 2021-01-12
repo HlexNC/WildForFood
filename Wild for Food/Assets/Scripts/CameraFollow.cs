2f887e6fc8e952ba01580175ccc08f63ef8861ed
HlexNC <rudaevalex@gmail.com>
2021-01-12 22:53:36 +0300

    Made a Scene, Added controller, Death and Run animation, Added Rigidbody, Added Obstacles

---
+++ Wild for Food/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(10, 10, 10);
+    }
+}
---
+++ Wild for Food/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public float turnSpeed;
+    private float _rightInput;
+    public bool gameOver;
+    public Animator playerAnim;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+    }
+    void FixedUpdate()
+    {
+        if (!gameOver)
+        {
+            _rightInput = Input.GetAxis("Horizontal");
+
+            transform.Translate(Vector3.forward * (Time.deltaTime * speed));
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * _rightInput);
+        }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+        }
+    }
+}
